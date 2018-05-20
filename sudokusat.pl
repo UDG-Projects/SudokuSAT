0a84cbb746078b8792bdb3d13152e0abe8178842
Marc Sanchez <u1939705@campus.udg.edu>
2018-05-20 13:16:29 +0200

    decideix i simplif done macarrone

--- sudokusat.pl
+++ sudokusat.pl
@@ -15,27 +15,66 @@ sat(CNF,I,M):-
     sat(... , ... ,M).
 
 
-%%%%%%%%%%%%%%%%%%	
+%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % decideix(F, Lit)
-% Donat una CNF, 
+% Donat una CNF,
 % -> el segon parametre sera un literal de CNF
 %  - si hi ha una clausula unitaria sera aquest literal, sino
 %  - un qualsevol o el seu negat.
+decideix([[A]], A).
+decideix([PL|CL], A) :- append([A], [], PL), !.
+decideix([PL|CL], A) :- CL \= [], decideix(CL, A), !.
+decideix([PL|CL], A) :- append([A],XS,PL).
 
-
-%%%%%%%%%%%%%%%%%%%%%
-% simlipf(Lit, F, FS)
-% Donat un literal Lit i una CNF, 
+%%%%%%%%%%%%%%%%%%%%%  DONE MACARRONE!!
+% simplif(Lit, CNF, CNFS)
+% Donat un literal Lit i una CNF,
 % -> el tercer parametre sera la CNF que ens han donat simplificada:
 %  - sense les clausules que tenen lit
 %  - treient -Lit de les clausules on hi es, si apareix la clausula buida fallara.
+simplif(_, [], []). % QUAN TINGUEM LA LLISTA BUIDA RETORNEM LA LLISTA BUIDA.
+simplif(Lit, [PRIMERA|CUA], CNFS) :- member(Lit, PRIMERA),  % Comprovem si conté lit a primera
+                                     simplif(Lit, CUA, CNFS), !. % seguim amb la cua.
+simplif(Lit, [PRIMERA|CUA], CNFS) :- X is 0-Lit, % neguem el literal i el fotem a x
+                                     append(A, [X|XS], PRIMERA), append(A, [XS], RES),  % Treure el literal negat a primera serà res
+                                     simplif(Lit, CUA, CUACNFS), % crida recursiva
+                                     append(RES, CUACNFS, CNFS), !. % Muntem la cadena final
+simplif(_, [PRIMERA|[]], [PRIMERA]). % Hem de filtrar la llista buida del final
+simplif(Lit, [PRIMERA|CUA], CNFS) :- simplif(Lit, CUA, CUACNFS), % Crida recursiva quan no conté Lit
+                                     append([PRIMERA], CUACNFS, CNFS), !. % Concatenar resultats
 
+%%%%%%%%%%%%%%
+% negat(A,ANEG)
+% Donat un literal enter torna el seu negat
+negat(A, ANEG) :- ANEG is 0-A.
+negat([A,B], ANEG) :- append([C], [D], ANEG), negat(A,C), negat(B,D).
 
+montaParelles([], []).
+montaParelles([A,B], PARELLA) :- negat([A,B], PARELLA).
+montaParelles(L, PARELLES) :- append([A], CUA, L), append([B], BCUA,CUA), append([A],BCUA, EXCLOUB),
+                              negat([A,B], PARELLA),
+                              montaParelles(EXCLOUB, PARELLESA),
+                              montaParelles(CUA, PARELLESB),
+                              append(PARELLA, PARELLASA, P), append(P, PARELLESB, PARELLES).
 
 %%%%%%%%%%%%%%%%%%%%%
 % exactamentUn(L,CNF)
 % Donat una llista de variables booleanes,
 % -> el segon parametre sera la CNF que codifica que exactament una sigui certa.
+% exactamentUn([A], CNF) :-  append([[A]], [[B]], CNF), negat(A,B). %  P ^ !P...
+% exactamentUn([A,B], CNF) :- append([[A,B]], [[C, D]], CNF), C is 0-A, D is 0-B.
+%exactamentUn([], []).
+%exactamentUn([A,B], CNF) :- negat([A,B], NEGAT), append([A,B], NEGAT, CNF).
+%exactamentUn(L, CNF) :- append([A], CUA, L), write(A), nl, exactamentUn(CUA, CNF).
+%exactamentUn(L, CNF) :- append(PRIMERS, ULTIMS, L),
+%exactamentUn(L, CNF) :- append([A], CUA, L), append([B], BCUA,CUA), append([A],BCUA, RESTA),
+%                        exactamentUn(RESTA , CNF), exactamentUn(CUA, CNF)
+%                        , negat(A,C), negat(B,D),
+%                        append(C,D,NEGATS).
+%exactamentUn(L, CNF) :- append(P, [A|CUA], L), append(P, A, R), append(R, [B|BCUA], CUA),
+%                        append(C, D, NEGATS), negat(A,C), negat(B,D),
+%                        append([L], [NEGATS], CNF).
+
 
 
 
@@ -91,8 +130,8 @@ codificaSudoku(N,Tauler,C0,CNF):-  kdominis(Tauler,C1),
                                    append(C0,C12,C012), append(C012,C34,CNF).
 
 
-								   
-%%%%%%%%%%%%%%%								 
+
+%%%%%%%%%%%%%%%
 % kdominis(T,F)
 % Donat un Tauler,
 % -> el segon parametre es la CNF que codifica els exactamentUn per cada casella (K-domini)
@@ -149,21 +188,3 @@ mostraFila(M,F,C,N):-   LB is (F-1)*N*N + (C-1)*N, UB is LB+N,
 
 % exemple sudoku 9x9
 % [c(1,4,2), c(1,5,6),c(1,7,7),c(1,9,1),c(2,1,6), c(2,2,8),c(2,5,7),c(2,8,9),c(3,1,1), c(3,2,9), c(3,6,4), c(3,7,5),c(4,1,8), c(4,2,2), c(4,4,1), c(4,8,4),c(5,3,4), c(5,4,6), c(5,6,2), c(5,7 ,9), c(6,2,5), c(6,6,3), c(6,8,2), c(6,9,8),c(7,3,9), c(7,4,3), c(7,8,7), c(7,9 ,4), c(8,2,4), c(8,5,5), c(8,8,3), c(8,9,6), c(9,1,7), c(9,3,3), c(9,5,1), c(9,6,8)]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
