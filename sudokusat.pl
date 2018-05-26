24bed5a6ff888c370ca2805b31c358d18025c4c6
Marc Sanchez <msanxes@gmail.com>
2018-05-26 13:43:33 +0200

    [FEAT] Fet fins tauler, tot funciona, no estem ornis!!! DONE MACARRONE

--- sudokusat.pl
+++ sudokusat.pl
@@ -12,7 +12,7 @@ sat(CNF,I,M):-
     simplif(Lit,CNF,CNFS),
 
     % crida recursiva amb la CNF i la interpretacio actualitzada
-	 append(I,[Lit],ISEG),
+	  append(I,[Lit],ISEG),
     sat(CNFS,ISEG ,M).
 
 
@@ -23,8 +23,8 @@ sat(CNF,I,M):-
 %  - si hi ha una clausula unitaria sera aquest literal, sino
 %  - un qualsevol o el seu negat.
 decideix(L,A):- append(_,[X|_], L), append([A],[],X),!. % detectar clàusula unitària
-decideix([PL|_], A) :- append([A],_,PL). % Retorna el primer element
-decideix([PL|_], B) :- append([A],_,PL), B is 0-A. % Retorna el negat del primer element
+decideix([PL|_], A) :- append([A],_,PL).                % Retorna el primer element
+decideix([PL|_], B) :- append([A],_,PL), B is 0-A.      % Retorna el negat del primer element
 
 
 %%%%%%%%%%%%%%%%%%%%%  DONE MACARRONE!!
@@ -33,26 +33,27 @@ decideix([PL|_], B) :- append([A],_,PL), B is 0-A. % Retorna el negat del primer
 % -> el tercer parametre sera la CNF que ens han donat simplificada:
 %  - sense les clausules que tenen lit
 %  - treient -Lit de les clausules on hi es, si apareix la clausula buida fallara.
-simplif(_, [], []). % QUAN TINGUEM LA LLISTA BUIDA RETORNEM LA LLISTA BUIDA.
-simplif(Lit, [PRIMERA|CUA], CNFS) :- member(Lit, PRIMERA),  							% Comprovem si conté lit a primera
-                                     simplif(Lit, CUA, CNFS), !. 						% seguim amb la cua.
-simplif(Lit, [PRIMERA|CUA], CNFS) :- X is 0-Lit, 										% neguem el literal i el fotem a x
-                                     append(A, [X|XS], PRIMERA), append(A, XS, RES),  	% Treure el literal negat a primera serà res
-                                     simplif(Lit, CUA, CUACNFS), 						% crida recursiva
-                                     append([RES], CUACNFS, CNFS), !. 					% Muntem la cadena final
-simplif(Lit, [PRIMERA|CUA], CNFS) :- simplif(Lit, CUA, CUACNFS), 						% Crida recursiva quan no conté Lit
-                                     append([PRIMERA], CUACNFS, CNFS), !. 				% Concatenar resultats
+simplif(_, [], []).                                                                    % QUAN TINGUEM LA LLISTA BUIDA RETORNEM LA LLISTA BUIDA.
+simplif(Lit, [PRIMERA|CUA], CNFS) :- member(Lit, PRIMERA),  							             % Comprovem si conté lit a primera
+                                     simplif(Lit, CUA, CNFS), !. 						           % seguim amb la cua.
+simplif(Lit, [PRIMERA|CUA], CNFS) :- X is 0-Lit, 										                   % neguem el literal i el fotem a x
+                                     append(A, [X|XS], PRIMERA), append(A, XS, RES),   % Treure el literal negat a primera serà res
+                                     simplif(Lit, CUA, CUACNFS), 						           % crida recursiva
+                                     append([RES], CUACNFS, CNFS), !. 					       % Muntem la cadena final
+simplif(Lit, [PRIMERA|CUA], CNFS) :- simplif(Lit, CUA, CUACNFS), 						           % Crida recursiva quan no conté Lit
+                                     append([PRIMERA], CUACNFS, CNFS), !. 				     % Concatenar resultats
 
-%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% DONE MACARRONE!!
 % negat(A,ANEG)
 % Donat una llista de literals enters torna una llista amb els literals enters negats.
 negat([],[]).
 negat([A],[ANEG]):- ANEG is 0-A,!.
 negat([A|CUA], [ANEG|CUANEG]):- negat([A],[ANEG]), negat(CUA,CUANEG).
 
-%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % combina(L,COMBINAT)
-% ...
+% COMBINAT és la llista resultant de combinar el primer element de L amb la resta
+% L -> és una llista d'elements a tractar
 combina([],[]).
 combina([A],[[A]]).
 combina([A,B],[[A,B]]).
@@ -60,9 +61,9 @@ combina(L, COMBINAT):- append([A,B], CUA, L),
 					   append([A],CUA,PIVOTA),
 					   combina(PIVOTA,ITSEG), append([[A,B]],ITSEG,COMBINAT),!.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % montaParelles(L, PARELLES)
-% ...
+% PARELLES és el resultat de combinar tots els elements de L entre sí.
 montaParelles([],[]).
 montaParelles([A,B],[[A,B]]):-!.
 montaParelles(L, PARELLES):- append([_],CUA,L),
@@ -71,7 +72,7 @@ montaParelles(L, PARELLES):- append([_],CUA,L),
 							 append(PIVOTA,ITSEG,PARELLES),!.
 
 
-%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % exactamentUn(L,CNF)
 % Donat una llista de variables booleanes,
 % -> el segon parametre sera la CNF que codifica que exactament una sigui certa.
@@ -80,48 +81,77 @@ exactamentUn([A],[[A]]).
 exactamentUn(L,CNF):- negat(L, LNEG), montaParelles(LNEG,PARELLES), append([L],PARELLES, CNF).
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % matExtreuPrimers(MAT, LLP, LLCUES)
 % Donada una matriu, retorna per separat la primera columna i la matriu restant.
 % -> MAT És la matriu que tractem
 % -> LLP És la llista amb els elements de la primera columna de mat
 % -> LLCUES És la matriu resultant a l'extracció.
-matExtreuPrimers([[]], [], []).
+matExtreuPrimers([], [], []).
 matExtreuPrimers(MAT, LLP, LLCUES):- append([PRL], MATCUA, MAT), append([PR], CUA, PRL),
                                      matExtreuPrimers(MATCUA, LLPRT, LLCUEST),
-                                     append(PR, LLPRT, LLP), append(CUA, LLCUEST, LLCUES).
+                                     append([PR], LLPRT, LLP), append([CUA], LLCUEST, LLCUES),!.
 
-%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% DONE MACARRONE!!
 % allDiff(L,F)
 % Donat una llista (L) de Kdominis,
 % -> CNF  codifica que no poden prendre el mateix valor.
-%allDiff([[]|_],[]).
-%allDiff(L,CNF):-
-
 % - Extraurem el primer literal de cada k-domini
 % - muntem una llista amb tots els literals extrets
 % - cridem a alldif amb totes les cues.
+allDiff([[]|_],[]).
+allDiff(L,CNF):- matExtreuPrimers(L, PCOL, MAT), negat(PCOL, PCOLNEG), montaParelles(PCOLNEG, PCOLNEGCOMB),
+                 allDiff(MAT, CNFPARCIAL), append(PCOLNEGCOMB, CNFPARCIAL, CNF),!.
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % taulerSudoku(N,C,Tauler,CNF)
 % Donat el domini de les caselles (N), una inicialitzacio del Sudoku (de forma [c[1,2,5],...]),
 % -> Tauler serà Llista de llistes de N-dominis del tauler,
 % -> i CNF codifica els valors assignats amb clausules unitaries que forcen valor a les caselles inicialitzades
-
 taulerSudoku(N,C,Tauler,CNF):-
     festauler(N,Tauler),
     inicialitzar(N,C,CNF).
 
+%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+% generaLLista(P, U, LL)
+% Genera una llista de números de P fins a U i ho posa a LL
+% -> P < U.
+% -> P és l'inici
+% -> U és el nombre final
+% -> LL és la llista d'enters de P fins a U. [P..U]
+generaLLista(P, P, [P]).
+generaLLista(P, U, LL):- P < U,  K is P+1, generaLLista(K, U, R), append([P], R, LL),!.
 
-%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+% treuN(N, L, EXTRETS, CUA)
+% Divideix L en EXTRETS i CUA, on EXTRETS conté N elements de L i CUA la resta.
+% N -> nombre d'elements a extreure
+% L -> Llista d'elements a tractar
+% EXTRETS -> N elements de L en forma de LLISTA
+% CUA -> L - els N primers elements de L en forma de LLISTA
+treuN(0, L, [], L).
+treuN(N, L, EXTRETS, CUA):- K is N-1, K >= 0, append([A], B, L), treuN(K, B, NOUEX, CUA), append([A], NOUEX, EXTRETS), !.
+
+%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+% separaPerN(N, L, R)
+% Donada una llista L d'elements, R serà L dividida en K parts de N elements cada part.
+% -> N és el nombre d'elements que han de tenir les subllistes
+% -> L és la llista a tractar
+% -> R és una llista de llistes de N elements cada subllista.
+separaPerN(_, [], []).
+separaPerN(N, L, R):- treuN(N, L, EXTRETS, CUA), separaPerN(N, CUA, NOUEXTRETS), append([EXTRETS], NOUEXTRETS, R), !.
+
+%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % festauler(N,T)
 % Donat el domini de les caselles (N),
 % -> T sera una llista de N llistes de N llistes de N nombres
 %    es a dir, una llista de files de N-dominis.
 %    El primer N-domini tindra la forma [1,2,...N] i aixi successivament de fila en fila fins
 %    a l'ultim N-domini corresponent a la casella (N,N) que sera [N*N*(N-1)+1,N*N*(N-1)+2,...N*N*N]
-
+% festauler(0, []). %F is N*N, K is F*N
+festauler(N, T):- K is N*N*N, generaLLista(1, K, LL), separaPerN(N, LL, LLN), separaPerN(N, LLN, T), !.
 
 %%%%%%%%%%%%%%%%%%%%%%
 % inicialitzar(N,LI,F)
@@ -137,7 +167,6 @@ taulerSudoku(N,C,Tauler,CNF):-
 % codificaSudoku(N,Tauler,C0,CNF)
 % Donat el domini de les caselles (N) d'un Sudoku, el seu tauler i la CNF que l'inicialitza (C0),
 % -> el quart parametre sera la CNF que codifica el Sudoku.
-
 codificaSudoku(N,Tauler,C0,CNF):-  kdominis(Tauler,C1),
                                    allDiffFiles(Tauler,C2),
                                    allDiffColumnes(Tauler,C3),
