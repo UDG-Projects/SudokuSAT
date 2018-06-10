3b2cdc792b8fbf231cfe632732c67adeb702bc76
Marc Sanchez <msanxes@gmail.com>
2018-06-10 17:43:46 +0200

    [FEAT] s'ha afegit la documentació de tots els predicats i hi ha un exemple de sudoku al sudoku1.txt

--- sudokusat.pl
+++ sudokusat.pl
@@ -15,41 +15,54 @@ sat(CNF,I,M):-
     sat(CNFS,ISEG ,M).
 
 
-%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+%%%%%%%%%%%%%%%%%%
 % decideix(F, Lit)
 % Donat una CNF,
 % -> el segon parametre sera un literal de CNF
 %  - si hi ha una clausula unitaria sera aquest literal, sino
 %  - un qualsevol o el seu negat.
-decideix(L,A):- append(_,[X|_], L), append([A],[],X),!. % detectar clàusula unitària
-decideix([PL|_], A) :- append([A],_,PL).                % Retorna el primer element
-decideix([PL|_], B) :- append([A],_,PL), B is 0-A.      % Retorna el negat del primer element
+                % detectar clàusula unitària
+decideix(L,A):- append(_,[X|_], L),
+                append([A],[],X),!.
+                        % Retorna el primer element
+decideix([PL|_], A) :- append([A],_,PL).
+                       % Retorna el negat del primer element
+decideix([PL|_], B) :- append([A],_,PL), B is 0-A.
 
 
-%%%%%%%%%%%%%%%%%%%%%  DONE MACARRONE!!
+%%%%%%%%%%%%%%%%%%%%%
 % simplif(Lit, CNF, CNFS)
 % Donat un literal Lit i una CNF,
 % -> el tercer parametre sera la CNF que ens han donat simplificada:
 %  - sense les clausules que tenen lit
 %  - treient -Lit de les clausules on hi es, si apareix la clausula buida fallara.
-simplif(_, [], []).                                                                    % QUAN TINGUEM LA LLISTA BUIDA RETORNEM LA LLISTA BUIDA.
-simplif(Lit, [PRIMERA|CUA], CNFS) :- member(Lit, PRIMERA),  							             % Comprovem si conté lit a primera
-                                     simplif(Lit, CUA, CNFS), !. 						           % seguim amb la cua.
-simplif(Lit, [PRIMERA|CUA], CNFS) :- X is 0-Lit, 										                   % neguem el literal i el fotem a x
-                                     append(A, [X|XS], PRIMERA), append(A, XS, RES),   % Treure el literal negat a primera serà res
-                                     simplif(Lit, CUA, CUACNFS), 						           % crida recursiva
-                                     append([RES], CUACNFS, CNFS), !. 					       % Muntem la cadena final
-simplif(Lit, [PRIMERA|CUA], CNFS) :- simplif(Lit, CUA, CUACNFS), 						           % Crida recursiva quan no conté Lit
-                                     append([PRIMERA], CUACNFS, CNFS), !. 				     % Concatenar resultats
+simplif(_, [], []). % QUAN TINGUEM LA LLISTA BUIDA RETORNEM LA LLISTA BUIDA.
+                                     % Comprovem si conté lit a primera
+simplif(Lit, [PRIMERA|CUA], CNFS) :- member(Lit, PRIMERA),
+                                     % seguim amb la cua.
+                                     simplif(Lit, CUA, CNFS), !.
+                                     % neguem el literal i el fotem a x
+simplif(Lit, [PRIMERA|CUA], CNFS) :- X is 0-Lit,
+                                     append(A, [X|XS], PRIMERA),
+                                     % Treure el literal negat a primera serà res
+                                     append(A, XS, RES),
+                                     % crida recursiva
+                                     simplif(Lit, CUA, CUACNFS),
+                                     % Muntem la cadena final
+                                     append([RES], CUACNFS, CNFS), !.
+                                     % Crida recursiva quan no conté Lit
+simplif(Lit, [PRIMERA|CUA], CNFS) :- simplif(Lit, CUA, CUACNFS),
+                                     % Concatenar resultats
+                                     append([PRIMERA], CUACNFS, CNFS), !.
 
-%%%%%%%%%%%%%% DONE MACARRONE!!
+%%%%%%%%%%%%%%
 % negat(A,ANEG)
 % Donat una llista de literals enters torna una llista amb els literals enters negats.
 negat([],[]).
 negat([A],[ANEG]):- ANEG is 0-A,!.
 negat([A|CUA], [ANEG|CUANEG]):- negat([A],[ANEG]), negat(CUA,CUANEG).
 
-%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+%%%%%%%%%%%%%%%%%%%%%
 % combina(L,COMBINAT)
 % COMBINAT és la llista resultant de combinar el primer element de L amb la resta
 % L -> és una llista d'elements a tractar
@@ -57,10 +70,11 @@ combina([],[]).
 combina([A],[[A]]).
 combina([A,B],[[A,B]]).
 combina(L, COMBINAT):- append([A,B], CUA, L),
-					   append([A],CUA,PIVOTA),
-					   combina(PIVOTA,ITSEG), append([[A,B]],ITSEG,COMBINAT),!.
+          					   append([A],CUA,PIVOTA),
+          					   combina(PIVOTA,ITSEG),
+                       append([[A,B]],ITSEG,COMBINAT),!.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % montaParelles(L, PARELLES)
 % PARELLES és el resultat de combinar tots els elements de L entre sí.
 montaParelles([],[]).
@@ -71,15 +85,17 @@ montaParelles(L, PARELLES):- append([_],CUA,L),
 							 append(PIVOTA,ITSEG,PARELLES),!.
 
 
-%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
+%%%%%%%%%%%%%%%%%%%%%
 % exactamentUn(L,CNF)
 % Donat una llista de variables booleanes,
 % -> el segon parametre sera la CNF que codifica que exactament una sigui certa.
 exactamentUn([],[]).
 exactamentUn([A],[[A]]).
-exactamentUn(L,CNF):- negat(L, LNEG), montaParelles(LNEG,PARELLES), append([L],PARELLES, CNF).
+exactamentUn(L,CNF):- negat(L, LNEG),
+                      montaParelles(LNEG,PARELLES),
+                      append([L],PARELLES, CNF).
 
-%%%%%%%%%%%%%% DONE MACARRONE!!
+%%%%%%%%%%%%%%
 % allDiff(L,F)
 % Donat una llista (L) de Kdominis,
 % -> CNF  codifica que no poden prendre el mateix valor.
@@ -95,8 +111,11 @@ allDiff(L,CNF):-matTransposa(L,T), iAllDiff(T,CNF).
 % Donada una llista de caselles L
 % -> CNF serà la clausla que valida que totes les caselles son diferents
 iAllDiff([],[]).
-iAllDiff(L,CNF):-append([PCOL],MAT,L), negat(PCOL, PCOLNEG), montaParelles(PCOLNEG, PCOLNEGCOMB),
-        iAllDiff(MAT, CNFPARCIAL), append(PCOLNEGCOMB, CNFPARCIAL, CNF),!.
+iAllDiff(L,CNF):- append([PCOL],MAT,L),
+                  negat(PCOL, PCOLNEG),
+                  montaParelles(PCOLNEG, PCOLNEGCOMB),
+                  iAllDiff(MAT, CNFPARCIAL),
+                  append(PCOLNEGCOMB, CNFPARCIAL, CNF),!.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % taulerSudoku(N,C,Tauler,CNF)
@@ -104,8 +123,10 @@ iAllDiff(L,CNF):-append([PCOL],MAT,L), negat(PCOL, PCOLNEG), montaParelles(PCOLN
 % -> Tauler serà Llista de llistes de N-dominis del tauler,
 % -> i CNF codifica els valors assignats amb clausules unitaries que forcen valor a les caselles inicialitzades
 taulerSudoku(N,C,Tauler,CNF):-
-    festauler(N,Tauler),
-    inicialitzar(N,C,CNF).
+  % Genera totes les possibles variables per cada domini dins de cada casella i cada llista.
+  festauler(N,Tauler),
+  % Agafa les caselles passades com a inputs C i genera la CNF que codifica aquestes caselles com a fixes.
+  inicialitzar(N,C,CNF).
 
 %%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % generaLLista(P, U, LL)
@@ -115,7 +136,9 @@ taulerSudoku(N,C,Tauler,CNF):-
 % -> U és el nombre final
 % -> LL és la llista d'enters de P fins a U. [P..U]
 generaLLista(P, P, [P]).
-generaLLista(P, U, LL):- P < U,  K is P+1, generaLLista(K, U, R), append([P], R, LL),!.
+generaLLista(P, U, LL):-  P < U,  K is P+1,
+                          generaLLista(K, U, R),
+                          append([P], R, LL),!.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % treuN(N, L, EXTRETS, CUA)
@@ -125,7 +148,10 @@ generaLLista(P, U, LL):- P < U,  K is P+1, generaLLista(K, U, R), append([P], R,
 % EXTRETS -> N elements de L en forma de LLISTA
 % CUA -> L - els N primers elements de L en forma de LLISTA
 treuN(0, L, [], L).
-treuN(N, L, EXTRETS, CUA):- K is N-1, K >= 0, append([A], B, L), treuN(K, B, NOUEX, CUA), append([A], NOUEX, EXTRETS), !.
+treuN(N, L, EXTRETS, CUA):- K is N-1, K >= 0,
+                            append([A], B, L),
+                            treuN(K, B, NOUEX, CUA),
+                            append([A], NOUEX, EXTRETS), !.
 
 %%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % separaPerN(N, L, R)
@@ -134,7 +160,9 @@ treuN(N, L, EXTRETS, CUA):- K is N-1, K >= 0, append([A], B, L), treuN(K, B, NOU
 % -> L és la llista a tractar
 % -> R és una llista de llistes de N elements cada subllista.
 separaPerN(_, [], []).
-separaPerN(N, L, R):- treuN(N, L, EXTRETS, CUA), separaPerN(N, CUA, NOUEXTRETS), append([EXTRETS], NOUEXTRETS, R), !.
+separaPerN(N, L, R):- treuN(N, L, EXTRETS, CUA),
+                      separaPerN(N, CUA, NOUEXTRETS),
+                      append([EXTRETS], NOUEXTRETS, R), !.
 
 %%%%%%%%%%%%%%%% DONE MACARRONE!!
 % festauler(N,T)
@@ -144,9 +172,12 @@ separaPerN(N, L, R):- treuN(N, L, EXTRETS, CUA), separaPerN(N, CUA, NOUEXTRETS),
 %    El primer N-domini tindra la forma [1,2,...N] i aixi successivament de fila en fila fins
 %    a l'ultim N-domini corresponent a la casella (N,N) que sera [N*N*(N-1)+1,N*N*(N-1)+2,...N*N*N]
 % festauler(0, []). %F is N*N, K is F*N
-festauler(N, T):- K is N*N*N, generaLLista(1, K, LL), separaPerN(N, LL, LLN), separaPerN(N, LLN, T), !.
+festauler(N, T):- K is N*N*N,
+                  generaLLista(1, K, LL),
+                  separaPerN(N, LL, LLN),
+                  separaPerN(N, LLN, T), !.
 
-%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % inicialitzar(N,LI,F)
 % Donat el domini de les caselles (N), i la llista d'inicialitzacions (de forma [c[1,2,5],...]),
 % -> el tercer parametre sera la CNF formada de clausules unitaries que forcen els valors corresponents
@@ -158,7 +189,7 @@ inicialitzar(N,LI,CNF):- append([c(F,C,D)],Resta,LI),
                          append([[POS]],W,CNF).
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DONE MACARRONE!!
 % codificaSudoku(N,Tauler,C0,CNF)
 % Donat el domini de les caselles (N) d'un Sudoku, el seu tauler i la CNF que l'inicialitza (C0),
 % -> el quart parametre sera la CNF que codifica el Sudoku.
@@ -177,8 +208,12 @@ codificaSudoku(N,Tauler,C0,CNF):-  kdominis(Tauler,C1),
 % -> el segon parametre es la CNF que codifica els exactamentUn per cada casella (K-domini)
 kdominis([],[]).
 kdominis([[]|T],F):-kdominis(T,F).
-kdominis(T,F):- append([PF],CUA,T), append([PE],CUAFILA,PF), exactamentUn(PE,CNF1),
-                append([CUAFILA],CUA,RESTA),kdominis(RESTA,CNFFINAL), append(CNF1,CNFFINAL,F),!.
+kdominis(T,F):- append([PF],CUA,T),
+                append([PE],CUAFILA,PF),
+                exactamentUn(PE,CNF1),
+                append([CUAFILA],CUA,RESTA),
+                kdominis(RESTA,CNFFINAL),
+                append(CNF1,CNFFINAL,F),!.
 
 
 %%%%%%%%%%%%%%%%%%%
@@ -208,7 +243,9 @@ matTransposa(MAT,RES):- append([PF],CUA,MAT),
 % -> EX: matUnio([[1]],[[2],[3]],[[1,2],[3]])
 matUnio([],T,T).
 matUnio(T,[],T).
-matUnio(ESQ,DRE,MAT):-append([PESQ],CUAESQ,ESQ), append([PDRE],CUADRE,DRE), append(PESQ,PDRE,PF),
+matUnio(ESQ,DRE,MAT):-append([PESQ],CUAESQ,ESQ),
+                      append([PDRE],CUADRE,DRE),
+                      append(PESQ,PDRE,PF),
                       matUnio(CUAESQ,CUADRE,MATRES),
                       append([PF],MATRES,MAT),!.
 
@@ -219,7 +256,7 @@ matUnio(ESQ,DRE,MAT):-append([PESQ],CUAESQ,ESQ), append([PDRE],CUADRE,DRE), appe
 transposarFila([],[]).
 transposarFila(L,RES):- append([P],CUA,L),
                         transposarFila(CUA,CUARES),
-                        append([[P]],CUARES,RES).
+                        append([[P]],CUARES,RES), !.
 
 %%%%%%%%%%%%%%%%%%%%%%
 % allDiffColumnes(T,F)
@@ -239,9 +276,12 @@ allDiffQuadrats(T,N,F):- iAllDiffQuadrats(T,N,0,F).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % iAllDiffQuadrats(T,N,M,F)
 % Inmersió per allDiffQuadrats.
-
 iAllDiffQuadrats(_,N,N,[]).
-iAllDiffQuadrats(T,N,M,F):- X is M+1, subset(T,N,M,S), allDiff(S,CNF), iAllDiffQuadrats(T,N,X,CNFX), append(CNF,CNFX,F),!.
+iAllDiffQuadrats(T,N,M,F):- X is M+1,
+                            subset(T,N,M,S),
+                            allDiff(S,CNF),
+                            iAllDiffQuadrats(T,N,X,CNFX),
+                            append(CNF,CNFX,F),!.
 
 %%%%%%%%%%%%%%%%%
 % subset(T,N,Q,S)
@@ -250,14 +290,17 @@ iAllDiffQuadrats(T,N,M,F):- X is M+1, subset(T,N,M,S), allDiff(S,CNF), iAllDiffQ
 % N és el valor del sudoku.
 % S Serà el subset referent al subquadrat de numero subquadrat.
 % -----------------
-% | 1 | 1 | 2 | 2 |
-% | 1 | 1 | 2 | 2 |
-% | 3 | 3 | 4 | 4 |
-% | 3 | 3 | 4 | 4 |
+% | 0 | 0 | 1 | 1 |
+% | 0 | 0 | 1 | 1 |
+% | 2 | 2 | 3 | 3 |
+% | 2 | 2 | 3 | 3 |
 % -----------------
 % SUBQUADRAT pot prendre valor de 1 - N on en la matriu expressada anteriorment cada valor
 % refereix al subquadrat marcat amb els nombres de 1 a 4.
-subset(T,N,SUBQUADRAT,S):- P is truncate(sqrt(N)), X is (SUBQUADRAT mod P)*P, Y is (SUBQUADRAT div P)*P, extreu(T,X,Y,N,S).
+subset(T,N,SUBQUADRAT,S):- P is truncate(sqrt(N)),
+                           X is (SUBQUADRAT mod P)*P,
+                           Y is (SUBQUADRAT div P)*P,
+                           extreu(T,X,Y,N,S).
 
 %%%%%%%%%%%%%%%%%%%%%
 % extreu(T,XI,YI,N,S)
@@ -267,7 +310,9 @@ subset(T,N,SUBQUADRAT,S):- P is truncate(sqrt(N)), X is (SUBQUADRAT mod P)*P, Y 
 % YI és columna inicial
 % N és el valor del SUDOKU
 % S serà el subquadrat de tamany SQRT(N) que està entre  XI - XI + SQRT(N) i  YI - YI + SQRT(N).
-extreu(T,XI,YI,N,S):-ARR is truncate(sqrt(N)), XF is XI + ARR, YF is YI+ARR, iExtreu(T,XI,YI,XF,YF,0,0,S).
+extreu(T,XI,YI,N,S):- ARR is truncate(sqrt(N)),
+                      XF is XI + ARR, YF is YI+ARR,
+                      iExtreu(T,XI,YI,XF,YF,0,0,S), !.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % iExtreu(T,XI,YI,XF,YF,X,Y,S)
@@ -276,14 +321,27 @@ extreu(T,XI,YI,N,S):-ARR is truncate(sqrt(N)), XF is XI + ARR, YF is YI+ARR, iEx
 % XI és Fila inici, XF és fila Final, X és Fila actual
 % YI és columna inici, YF és columna Final, Y és columna actual
 % S és el subquadrat format per tots els elements entre XI i XY, YI i YF.
-iExtreu(_,_,_,_,YF,_,YF,[]). %ja ha trobat tot el quadrat
+
+% ja ha trobat tot el quadrat
+iExtreu(_,_,_,_,YF,_,YF,[]):-!.
+% Encara no he arribat a la fila que cerco.
+iExtreu(T,XI,YI,XF,YF,_,Y,S):-    Y < YI, append([_],CUAF,T), YSEG is Y+1,
+                                  iExtreu(CUAF,XI,YI,XF,YF,0,YSEG,S).
+% me passat de columnes (X) puc saltar a la linia seguent
 iExtreu(T,XI,YI,XF,YF,XF,Y,S):-   append([_],CUAF,T), YSEG is Y+1,
-                                  iExtreu(CUAF,XI,YI,XF,YF,0,YSEG,S).   % me passat de columnes (X) puc saltar a la linia seguent
+                                  iExtreu(CUAF,XI,YI,XF,YF,0,YSEG,S).
+% No me passat ni de files ni de columnes i miro si estic dins del rang (extrec)
 iExtreu(T,XI,YI,XF,YF,X,Y,S):-    append([PF],CUAF,T), append([PC], CUAC, PF),
-                                  dinsRang(XI,XF,YI,YF,X,Y), append([CUAC],CUAF,MAT), XSEG is X+1,
-                                  iExtreu(MAT,XI,YI,XF,YF,XSEG,Y,Q), append([PC],Q,S),!.
-iExtreu(T,XI,YI,XF,YF,X,Y,S):-    append([PF],CUAF,T), append([_], CUAC, PF), append([CUAC],CUAF,MAT),  X<XF, XS is X+1,
+                                  dinsRang(XI,XF,YI,YF,X,Y),
+                                  append([CUAC],CUAF,MAT), XSEG is X+1,
+                                  iExtreu(MAT,XI,YI,XF,YF,XSEG,Y,Q),
+                                  append([PC],Q,S),!.
+% Encara no he trobat l'inici del rang i vaig passant elements
+iExtreu(T,XI,YI,XF,YF,X,Y,S):-    append([PF],CUAF,T),
+                                  append([_], CUAC, PF),
+                                  append([CUAC],CUAF,MAT),  X<XF, XS is X+1,
                                   iExtreu(MAT,XI,YI,XF,YF,XS,Y,S).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % dinsRang(XI,XF,YI,YF,X,Y)
@@ -299,7 +357,9 @@ dinsRang(XI,XF,YI,YF,X,Y):-  X>=XI, X<XF, Y>=YI, Y<YF.
 % -> es mostra la solucio per pantalla si en te o es diu que no en te.
 resol(N,Inputs):- nl, write('NEM A RESOLDRE EL SUDOKU : '),nl,
                   write('....................................'),nl,
-                  mostraSudoku(N,Inputs), taulerSudoku(N, Inputs, T, C0), codificaSudoku(N,T,C0,CNF),
+                  mostraSudoku(N,Inputs),
+                  taulerSudoku(N, Inputs, T, C0),
+                  codificaSudoku(N,T,C0,CNF),
                   sat(CNF,[],M), mostra(M,N),!.
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -347,15 +407,17 @@ iPintaSeparador(N, NF):- write('-'), NSEG is N+1, iPintaSeparador(NSEG, NF).
 
 % Donat un Model per a un encoding d'un Sudoku de NxN, ens mostra els valors finals.
 % (s'assumeix que s'han codificat els K-dominis, etc com es requereix a la practica).
-mostra(M,N):- write('....................................'),nl, pintaSeparador(N), mostraM(M,1,N).
+mostra(M,N):- write('....................................'),nl,
+              pintaSeparador(N), mostraM(M,1,N).
 
 mostraM(_,F,N):-N is F-1,!.
-mostraM(M,F,N):-mostraFila(M,F,1,N), write('|'), nl, pintaSeparador(N), F1 is F+1, mostraM(M,F1,N).
+mostraM(M,F,N):-mostraFila(M,F,1,N), write('|'), nl,
+                pintaSeparador(N), F1 is F+1, mostraM(M,F1,N).
 
 mostraFila(_,_,C,N):- C>N,!.
-mostraFila(M,F,C,N):-   LB is (F-1)*N*N + (C-1)*N, UB is LB+N,
-                        member(X,M),X>LB, X=<UB, V is X-LB, write('|'), write(V),
-                        Cp is C+1, mostraFila(M,F,Cp,N).
+mostraFila(M,F,C,N):- LB is (F-1)*N*N + (C-1)*N, UB is LB+N,
+                      member(X,M),X>LB, X=<UB, V is X-LB, write('|'), write(V),
+                      Cp is C+1, mostraFila(M,F,Cp,N).
 
 
 
